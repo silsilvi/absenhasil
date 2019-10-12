5d198f1e5d27606b75fad787ba5f603e3a02ada3
ANIS1805 <anisrusnaaifah@gmail.com>
2019-10-12 16:33:05 +0700

    anis

--- absensanner.php
+++ absensanner.php
@@ -303,7 +303,7 @@ if(isset($_POST["Import"])){
 		  	$file = fopen($filename, "r");
 	        while(($getData = fgetcsv($file, 10000, ",")) !== FALSE){
 				$sql = "INSERT into absensi (kodeabsen,tanggal,kodep,Nama,jamhadir,jampulang,jamhadir_bulat,jampulang_bulat,ketr,telat,lembur,jamkerja) 
-				values ('".$getData[0]."','".$getData[1]."','".$getData[2]."','".$getData[3]."','".$getData[4]."','".$getData[5]."','".$getData[6]."','".$getData[7]."','".$getData[8]."','".$getData[9]."','".$getData[10].",'".$getData[11]."')";
+				values ('".$getData[0]."','".$getData[1]."','".$getData[2]."','".$getData[3]."','".$getData[4]."','".$getData[5]."','".$getData[6]."','".$getData[7]."','".$getData[8]."','".$getData[9]."','".$getData[10]."','".$getData[11]."')";
                 $result = mysqli_query($koneksi, $sql);
 					if(!isset($result)){	
 						echo "<script type=\"text/javascript\">
