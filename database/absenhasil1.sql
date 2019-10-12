c7893b33f018887c31d525d252c7984308634acd
ANIS1805 <anisrusnaaifah@gmail.com>
2019-10-12 16:35:05 +0700

    anis

--- database/absenhasil1.sql
+++ database/absenhasil1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 12, 2019 at 09:32 AM
+-- Generation Time: Oct 12, 2019 at 11:34 AM
 -- Server version: 10.4.6-MariaDB
 -- PHP Version: 7.3.9
 
@@ -25,29 +25,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `absen`
---
-
-CREATE TABLE `absen` (
-  `kodeabsen` int(10) NOT NULL,
-  `tanggal` date DEFAULT NULL,
-  `jamhadir` time DEFAULT NULL,
-  `jampulang` time DEFAULT NULL,
-  `kodep` int(10) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `absen`
---
-
-INSERT INTO `absen` (`kodeabsen`, `tanggal`, `jamhadir`, `jampulang`, `kodep`) VALUES
-(1249, '2019-10-07', '06:10:00', '21:00:00', 1),
-(1250, '2019-10-08', '08:00:00', '17:00:00', 2),
-(1252, '2019-10-09', '09:00:00', '21:00:00', 1111);
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `absensi`
 --
 
@@ -65,6 +42,28 @@ CREATE TABLE `absensi` (
   `lembur` float DEFAULT NULL,
   `jamkerja` float DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `absensi`
+--
+
+INSERT INTO `absensi` (`kodeabsen`, `tanggal`, `kodep`, `Nama`, `jamhadir`, `jampulang`, `jamhadir_bulat`, `jampulang_bulat`, `ketr`, `telat`, `lembur`, `jamkerja`) VALUES
+(275, '2019-10-04', 1111, 'Silvi', '13:19:15', '13:19:24', '00:00:00', '00:00:00', 'Telat', 7.3, 0, 0),
+(276, '2019-10-04', 3333, 'tr', '13:19:55', '13:19:59', '00:00:00', '13:00:00', 'Telat', 7.3, 0, 0),
+(277, '2019-10-04', 2222, 'Unc', '16:33:59', '16:34:13', '00:00:00', '16:30:00', 'Telat', 10.6, 0, 0),
+(1119, '2019-10-09', 1111, '', '10:30:29', '13:06:00', '11:00:00', '13:00:00', 'Telat', 5, 0, 2.6),
+(1128, '2019-10-09', 1205, '', '13:48:42', '13:45:14', '14:00:00', '13:30:00', 'Telat', -436257, 0, 0),
+(1129, '2019-10-09', 2222, 'Unc', '14:17:27', '00:00:00', '14:30:00', '00:00:00', 'Telat', 8.5, 0, 0),
+(1154, '2019-10-10', 6107, 'Ima Indriyati', '16:00:15', '15:59:13', '16:30:00', '15:30:00', '', 0, 0, 0),
+(1254, '2019-10-11', 6107, 'Ima Indriyati', '16:02:13', '17:49:09', '16:30:00', '17:30:00', 'Ontime', 0, 11.5, 0),
+(1255, '2019-10-14', 6107, '', '16:02:45', '16:02:35', '16:30:00', '16:00:00', 'Ontime', 0, 0, 0),
+(1272, '2019-10-12', 1111, '', '00:00:00', '13:15:58', '00:00:00', '13:00:00', '', 0, 0, 0),
+(1273, '2019-10-12', 2222, '', '00:00:00', '17:23:20', '00:00:00', '17:00:00', '', 0, 0, 0),
+(1274, '2019-10-12', 1205, '', '00:00:00', '18:23:42', '00:00:00', '18:00:00', '', 0, 1, 0),
+(1276, '2019-10-12', 3333, '', '16:42:07', '13:41:43', '16:30:00', '13:30:00', 'Telat', 10.5, 0, -3),
+(1277, '2019-10-12', 5555, '', '00:00:00', '16:57:07', '00:00:00', '16:30:00', '', 0, 0, 0),
+(1278, '2019-10-12', 6104, 'Denik', '08:22:42', '14:23:24', '08:30:00', '14:00:00', 'Telat', 2.5, 0, 6),
+(1283, '2019-10-12', 6107, 'Ima Indriyati', '18:12:37', '07:13:09', '18:30:00', '07:00:00', 'Telat', 1.5, 1, 0);
 
 -- --------------------------------------------------------
 
@@ -184,6 +183,29 @@ INSERT INTO `pegawai` (`kodep`, `Nama`, `alamat`, `jeniskelamin`, `notelp`, `kod
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tabsen`
+--
+
+CREATE TABLE `tabsen` (
+  `kodeabsen` int(10) NOT NULL,
+  `tanggal` date DEFAULT NULL,
+  `jamhadir` time DEFAULT NULL,
+  `jampulang` time DEFAULT NULL,
+  `kodep` int(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tabsen`
+--
+
+INSERT INTO `tabsen` (`kodeabsen`, `tanggal`, `jamhadir`, `jampulang`, `kodep`) VALUES
+(1249, '2019-10-07', '06:10:00', '21:00:00', 1),
+(1250, '2019-10-08', '08:00:00', '17:00:00', 2),
+(1252, '2019-10-09', '09:00:00', '21:00:00', 1111);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tembel`
 --
 
@@ -279,12 +301,6 @@ INSERT INTO `user` (`id`, `username`, `password`) VALUES
 --
 
 --
--- Indexes for table `absen`
---
-ALTER TABLE `absen`
-  ADD PRIMARY KEY (`kodeabsen`);
-
---
 -- Indexes for table `absensi`
 --
 ALTER TABLE `absensi`
@@ -327,6 +343,12 @@ ALTER TABLE `pegawai`
   ADD PRIMARY KEY (`kodep`);
 
 --
+-- Indexes for table `tabsen`
+--
+ALTER TABLE `tabsen`
+  ADD PRIMARY KEY (`kodeabsen`);
+
+--
 -- Indexes for table `tembel`
 --
 ALTER TABLE `tembel`
@@ -359,12 +381,6 @@ ALTER TABLE `user`
 --
 -- AUTO_INCREMENT for dumped tables
 --
-
---
--- AUTO_INCREMENT for table `absen`
---
-ALTER TABLE `absen`
-  MODIFY `kodeabsen` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1253;
 
 --
 -- AUTO_INCREMENT for table `dryyer`
@@ -401,6 +417,12 @@ ALTER TABLE `lain`
 --
 ALTER TABLE `pegawai`
   MODIFY `kodep` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2147483648;
+
+--
+-- AUTO_INCREMENT for table `tabsen`
+--
+ALTER TABLE `tabsen`
+  MODIFY `kodeabsen` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1253;
 
 --
 -- AUTO_INCREMENT for table `tembel`
